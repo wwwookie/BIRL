3b83176a751a55a4c3824b7ecbc395c778168266
wwwookie <alwin-schuster@gmx.at>
2021-09-09 19:49:53 +0200

    adjusted structure [see uml]

---
+++ game/src/main/java/birl/Model/Character.java
@@ -0,0 +1,10 @@
+package birl.Model;
+
+public class Character {
+    
+    protected int id;
+    protected double lvlScaling;
+    protected Ability ability;  // TODO: replace this with just a text String
+    protected Type type;
+    protected String name;
+}
