fad6ed9d8dc9f24b975e2cf7f392b3a308a43621
wwwookie <alwin-schuster@gmx.at>
2021-11-15 21:45:37 +0100

    added db connection, not tested yet

--- game/src/main/java/birl/Main.java
+++ game/src/main/java/birl/Main.java
@@ -2,4 +2,12 @@ package birl;
 
 public class Main {
     
+    // test
+    DataBaseManager dbm = new DataBaseManager(
+        "jdbc:postgresql://localhost:5432/birl",
+        "wwwookie",
+        "supersecurepwd",
+        "SELECT * FROM type");
+
+    dbm.connectToDB();      // TODO: fix error here
 }
