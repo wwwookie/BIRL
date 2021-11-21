add9a4f429f9c8687e7d445c5ca48075efb2fc81
wwwookie <alwin-schuster@gmx.at>
2021-11-21 21:21:52 +0100

    added test class for db, empty rn

---
+++ game/src/test/java/birl/DataBaseManagerTest.java
@@ -0,0 +1,13 @@
+package birl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import birl.DataBaseManager;
+
+public class DataBaseManagerTest {
+    
+    // TODO: write
+}
