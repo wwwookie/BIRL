137cbe3fd5da3fbc47e10ea07e5d74e40801e060
wwwookie <alwin-schuster@gmx.at>
2021-12-13 12:59:03 +0100

    added minimal DB tests

--- game/src/test/java/birl/DataBaseManagerTest.java
+++ game/src/test/java/birl/DataBaseManagerTest.java
@@ -5,9 +5,25 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import birl.DataBaseManager;
-
 public class DataBaseManagerTest {
     
     // TODO: write
+    DataBaseManager dbm = new DataBaseManager(
+            "jdbc:postgresql://localhost:5432/birl",
+            "wwwookie",
+            "supersecurepwd");
+
+    @BeforeEach
+    void setUp(){
+    }
+
+    @Test
+    public void getNameFromTableCorrectInput(){
+        assertEquals("Drunk Douchebag", dbm.selectNameFromTableByID("character", 1));
+    }
+
+    @Test
+    public void getNameFromTableNonExistingID(){
+        assertEquals(null, dbm.selectNameFromTableByID("character", 0));
+    }
 }
