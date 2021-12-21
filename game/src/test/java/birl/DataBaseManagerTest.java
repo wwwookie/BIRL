c72f7edd9639dcd346d68ff92b09a7478900711f
wwwookie <alwin-schuster@gmx.at>
2021-12-21 17:25:57 +0100

    added necessary import

--- game/src/test/java/birl/DataBaseManagerTest.java
+++ game/src/test/java/birl/DataBaseManagerTest.java
@@ -5,6 +5,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import birl.Controller.DataBaseManager;
+
 public class DataBaseManagerTest {
     
     // TODO: write
