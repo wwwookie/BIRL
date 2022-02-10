c1e779ad03e7255e6955d9972ee4993b73e3f8b5
wwwookie <alwin-schuster@gmx.at>
2022-02-10 18:35:30 +0100

    added test [checkFirstThreeNames()]

--- game/src/test/java/birl/TextFileHandlerTest.java
+++ game/src/test/java/birl/TextFileHandlerTest.java
@@ -1,10 +1,11 @@
-/** Unit tests regarding Peron.java here. */
+/** Unit tests regarding TextFileHandler.java here. */
 
 package birl;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
+import java.util.ArrayList;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -16,15 +17,19 @@ import birl.Model.Character;
 public class TextFileHandlerTest {
 
     File homeDir = new File(System.getProperty("user.home"));
-    File characters = new File(homeDir, "Desktop/BIRL/game/data/characters/characters_test.txt");
+    File characters = new File(homeDir, "Desktop/BIRL/game/data/characters/characters.txt");
     File gears = new File(homeDir, "Desktop/BIRL/game/data/gears/gears.txt");
-    TextFileHandler tfh = new TextFileHandler(characters, gears);
+    File moves = new File(homeDir, "Desktop/BIRL/game/data/moves/moves.txt");
+    File abilities = new File(homeDir, "Desktop/BIRL/game/data/abilities/abilities.txt");
+    File effects = new File(homeDir, "Desktop/BIRL/game/data/effects/effects.txt");
+
+    TextFileHandler tfh = new TextFileHandler(characters, gears, moves, abilities, effects);
 
     @BeforeEach
     void setUp(){}
 
     @Test
-    public void checkCharacterFromTestfile(){
+    public void checkFirstCharacterFromTestfile(){
         Character c = tfh.getCharacterList().get(0);
 
         assertEquals(1, c.getId());
@@ -34,4 +39,15 @@ public class TextFileHandlerTest {
         assertEquals("This guy is that drunk, he cannot even hold his beer.", c.getAbout());
 
     }
+
+    @Test
+    public void checkFirstThreeNamesFromTestFile(){
+        ArrayList <Character> characterList = tfh.getCharacterList();
+
+        assertEquals("Drunk Douchebag", characterList.get(0).getName());
+        assertEquals("Workaholic", characterList.get(1).getName());
+        assertEquals("Gamer Guy", characterList.get(2).getName());
+    }
+
+    // TODO: more testing
 }
