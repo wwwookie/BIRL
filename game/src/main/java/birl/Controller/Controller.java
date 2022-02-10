9203e6116cf9e6b16aafc43a972ccc250e72200a
wwwookie <alwin-schuster@gmx.at>
2022-02-10 18:30:18 +0100

    added TextFileHandler initialization [tfh now used and initiated]

--- game/src/main/java/birl/Controller/Controller.java
+++ game/src/main/java/birl/Controller/Controller.java
@@ -1,11 +1,68 @@
 package birl.Controller;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import birl.Model.Character;
+import birl.Model.Gear;
+import birl.Model.Move;
+import birl.Model.Ability;
+import birl.Model.Effect;
+
 // is one controller-class enough to handle whole application?
 // TODO: implement
 // View updates Controller with user-actions
 // Controller work with model-instances, manages data
 // Model is manipulated
 
+/**
+ *  Handles all data manipulation.
+ */
+
 public class Controller {
     
+    ArrayList <Character> characterList;
+    ArrayList <Gear> gearList;
+    ArrayList <Move> moveList;
+    ArrayList <Ability> abilityList;
+    ArrayList <Effect> effectList;
+
+    File homeDir;
+    File characterFile;
+    File gearFile;
+    File moveFile;
+    File abilityFile;
+    File effectFile;
+    
+    TextFileHandler tfh;
+
+    public Controller(){
+
+        init();
+
+        // manipulat data
+    }
+
+    private void init(){
+
+        homeDir = new File(System.getProperty("user.home"));
+        characterFile = new File(homeDir, "Desktop/BIRL/game/data/characters/characters.txt");
+        gearFile = new File(homeDir, "Desktop/BIRL/game/data/gears/gears.txt");
+        moveFile = new File(homeDir, "Desktop/BIRL/game/data/moves/moves.txt");
+        abilityFile = new File(homeDir, "Desktop/BIRL/game/data/abilities/abilities.txt");
+        effectFile = new File(homeDir, "Desktop/BIRL/game/data/effects/effects.txt");
+
+        tfh  = new TextFileHandler(characterFile, gearFile, moveFile, abilityFile, effectFile);
+
+        extractAllDataFromFiles(tfh);
+    }
+
+    private void extractAllDataFromFiles(TextFileHandler tfh){
+
+        characterList = tfh.getCharacterList();
+        gearList = tfh.getGearList();
+        moveList = tfh.getMoveList();
+        abilityList = tfh.getAbilityList();
+        effectList = tfh.getEffectList();
+    }
 }
