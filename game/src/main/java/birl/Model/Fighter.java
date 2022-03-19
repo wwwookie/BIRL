193b848eae453f7da50f6b69f12ca543632eec90
wwwookie <alwin-schuster@gmx.at>
2022-03-19 22:47:49 +0100

    fighters can now attack each other[using gear/moves]

--- game/src/main/java/birl/Model/Fighter.java
+++ game/src/main/java/birl/Model/Fighter.java
@@ -4,11 +4,12 @@ public class Fighter extends Character{
 
     private int fighterId;
     private int lvl;
-    private int hp;
+    private int baseHP;
+    private int actualHP;
     private Move [] moves = new Move[3];
     private Gear gear;
 
-    public Fighter(int fighterId, int lvl, int hp, int characterId, String name, double lvlScaling, String type, String about) {
+    public Fighter(int fighterId, int lvl, int baseHP, int characterId, String name, double lvlScaling, String type, String about) {
         super(characterId, name, lvlScaling, type, about);
 
         this.fighterId = fighterId;       // TODO: check if id is already existing in DB
@@ -17,7 +18,7 @@ public class Fighter extends Character{
             this.lvl = lvl;
         else System.err.println("Lvl must be greater than 0 and smaller than 101.");
 
-        this.hp = hp;
+        this.baseHP = baseHP;
     }
 
     /** @return this fighters moves. */
@@ -41,9 +42,50 @@ public class Fighter extends Character{
     /** @return this fighters equiped gear. */
     public Gear getGear(){  return gear;   }
 
-    /** Euqips this fighter with @param gear. */
+    /** Equips this fighter with @param gear. */
     public void setGear(Gear gear){   this.gear = gear;    }
 
     /** Unequips this fighters gear. */
     public void removeGear(){   gear = null; }
+
+    /**
+     * Manipulates this fighters actualHP.
+     * @param value - hp will be de-/increased by this value
+     * Negative values will decrease, positive values will increase.
+     */
+    public void manipulateHP(int value){
+
+        // fighters can heal to a maximum of baseHP
+        if(actualHP + value > baseHP)
+            actualHP = baseHP;
+        else actualHP += value;
+    }
+
+    /**
+     * This fighter uses a move or a gear on the given target.
+     * @param moveOrGear - from this fighters moves or gear
+     * @param target - to be attacked fighter
+     * @throws Exception - if move or gear not learnd
+     */
+
+    public void use(int moveOrGearID, Fighter target) throws Exception{
+
+        if(moveOrGearID == gear.getId()){
+            target.manipulateHP(gear.getDamage());
+            manipulateHP(gear.getHeal());
+        } else if(moveOrGearID == moves[0].getId()){
+            target.manipulateHP(moves[0].getDamage());
+            manipulateHP(moves[0].getHeal());
+        } else if(moveOrGearID == moves[1].getId()){
+            target.manipulateHP(moves[1].getDamage());
+            manipulateHP(moves[1].getHeal());
+        } else if(moveOrGearID == moves[2].getId()){
+            target.manipulateHP(moves[2].getDamage());
+            manipulateHP(moves[2].getHeal());
+        } else throw new Exception("This fighter doesn't know this move or gear.");
+
+        //TODO: print used move/gear to game's console
+        // ... also make "effectivity" work
+
+    }
 }
