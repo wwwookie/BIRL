c6f16e4c8a4ff1df1916dc6be4b37dee4a34c3c4
wwwookie <alwin-schuster@gmx.at>
2022-03-24 21:18:46 +0100

    modified test file[works with json now]

---
+++ game/src/test/java/birl/JsonFileHandlerTest.java
@@ -0,0 +1,62 @@
+/** Unit tests regarding TextFileHandler.java here. */
+
+package birl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+import birl.Controller.JsonFileHandler;
+import birl.Model.Character;
+import birl.Model.Gear;
+import birl.Model.Move;
+
+
+public class JsonFileHandlerTest {
+
+    JsonFileHandler jfh = new JsonFileHandler();
+
+    @Test
+    public void checkFirstCharacterFromCharacters(){
+        Character c = jfh.getCharacters()[0];
+
+        assertEquals(1, c.getId());
+        assertEquals("Drunk Douchebag", c.getName());
+        assertEquals(0.0, c.getLvlScaling());
+        assertEquals("Human", c.getType());
+        assertEquals("This guy is that drunk, he cannot even hold his beer.", c.getAbout());
+
+    }
+
+    @Test
+    public void checkFirstThreeNamesFromCharacters(){
+
+        assertEquals("Drunk Douchebag", jfh.getCharacters()[0].getName());
+        assertEquals("Workaholic", jfh.getCharacters()[1].getName());
+        assertEquals("Gamer Guy", jfh.getCharacters()[2].getName());
+    }
+
+    @Test
+    public void checkFirstGearFromGears(){
+        Gear g = jfh.getGears()[0];
+
+        assertEquals(0, g.getId());
+        assertEquals("Test-Gear", g.getName());
+        assertEquals(1, g.getDamage());
+        assertEquals(1, g.getHeal());
+        assertEquals("Human", g.getType());
+        assertEquals("This gear is useless..", g.getEffect());
+    }
+
+    @Test
+    public void checkFirstMoveFromMoves(){
+        Move m = jfh.getMoves()[0];
+
+        assertEquals(0, m.getId());
+        assertEquals("Test-Move", m.getName());
+        assertEquals(1, m.getDamage());
+        assertEquals(1, m.getHeal());
+        assertEquals("Human", m.getType());
+        assertEquals("This move does nothing..", m.getEffect());
+    }
+}
